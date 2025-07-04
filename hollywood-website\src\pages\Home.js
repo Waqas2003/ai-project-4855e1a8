66bcad00033516b4d58aa328b956ce3080457543
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:35:47 +0500

    Add hollywood-website\src\pages\Home.js

---
+++ hollywood-website\src\pages\Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Hollywood Website</h1>
+      <p>Explore the latest news, movies, and celebrities in Hollywood.</p>
+    </div>
+  );
+}
+
+export default Home;
